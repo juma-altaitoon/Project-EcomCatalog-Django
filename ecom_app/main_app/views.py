27a0a322b02e7b042b8f22345bd0aaa923612993
maithamaraibi <maytham.aloraibi@gmail.com>
2023-01-08 13:51:53 +0300

    about and landing page

--- ecom_app/main_app/views.py
+++ ecom_app/main_app/views.py
@@ -12,3 +12,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 def home(request):
     # return HttpResponse('<h1>Hello<h1>')
     return render(request, 'home.html')
+
+def about(request):
+    return render(request,'about.html' )
