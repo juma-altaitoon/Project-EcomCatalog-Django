052ac58247dea098153dc904a7a755dd1ad25a54
nusaibaalawadhi <n_f98@hotmail.com>
2023-01-08 12:29:43 +0300

    .env and sitting

--- ecom_app/ecom_app/settings.py
+++ ecom_app/ecom_app/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,9 +81,9 @@ WSGI_APPLICATION = 'ecom_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecom_app',
-        'USER': 'maithamalaraibi',
-        'PASSWORD':'123456789'
+        'NAME': os.getenv('DATABASENAME'),
+        'USER': os.getenv('DATABASEUSER'),
+        'PASSWORD':os.getenv('DATABASEPASSWORD')
     }
 }
 
