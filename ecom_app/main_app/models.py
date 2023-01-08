b676ddbedcc479f6013346423dff438c245c68c8
juma-a <juma.altaitoon@gmail.com>
2023-01-08 10:31:09 +0300

    Project 3 - ecom_app : 1st commit

---
+++ ecom_app/main_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+# Create your models here.
---
+++ ecom_app/main_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Import models
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Hello<h1>')
