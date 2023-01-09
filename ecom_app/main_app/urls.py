4c8938c398393b1607a897a1524e6cbcaf635657
maithamaraibi <maytham.aloraibi@gmail.com>
2023-01-09 15:03:58 +0300

    pages and style

--- ecom_app/main_app/urls.py
+++ ecom_app/main_app/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/', views.order, name='profile'),
     path('about/', views.about, name='about'),
     path('order/', views.order, name='order'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('main_app/users/', views.profile, name='profile'),
+    path('products/products/', views.products, name='products'),
 ]
